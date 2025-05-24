3879ac372a33a8bcdf016d598509a869ee442330
Otakukorean <bboomstation1@gmail.com>
2025-05-24 03:37:45 +0300

    some edits

--- components/portfoli.tsx
+++ components/portfoli.tsx
@@ -216,22 +216,16 @@ export default function PortfolioV2() {
 
                 <div className="mt-12 flex gap-4">
                   <a
-                    href="#"
+                    href="https://github.com/Otakukorean"
                     className="p-2 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 hover:text-teal-500 dark:hover:text-teal-400 transition-colors"
                   >
                     <Github size={20} />
                   </a>
                   <a
-                    href="#"
+                    href="https://www.linkedin.com/in/ahmad-mustafa-33860a313"
                     className="p-2 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 hover:text-teal-500 dark:hover:text-teal-400 transition-colors"
                   >
                     <Linkedin size={20} />
-                  </a>
-                  <a
-                    href="#"
-                    className="p-2 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 hover:text-teal-500 dark:hover:text-teal-400 transition-colors"
-                  >
-                    <Mail size={20} />
                   </a>
                 </div>
               </motion.div>
